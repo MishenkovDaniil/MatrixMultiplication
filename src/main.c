99be8d88a92af09e0d935b7ce95b9c2c669dbfd9
Mishenkov Daniil <mishenkovdaniil@mail.ru>
2025-10-04 14:57:29 +0300

    add time measuring for parallel strategy

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@
 
 #define N 1000
 
+static double diff_sec(struct timespec a, struct timespec b) {
+    return (a.tv_sec - b.tv_sec) + (a.tv_nsec - b.tv_nsec) / 1e9;
+}
+
 static void run_example1(int n)
 {
     struct Matrix *A = matrix_generate(n, n, 1000);
@@ -29,33 +33,44 @@ static void run_example2(int n)
     struct Matrix *B = matrix_generate(N, N, 1000);
     struct Matrix *C = matrix_ctor(N, N);
 
-    clock_t start = clock();
+    struct timespec t0, t1;
 
+    clock_gettime(CLOCK_MONOTONIC, &t0);
     mul_matrices_bad2(A, B, C);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
 
-    clock_t end = clock();
-    float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+    double seconds = diff_sec(t1, t0);
 
-    printf("[Bad] N = %d, Elapsed time = %f.\n", N, seconds);
+    printf("[Bad] N = %d, Elapsed time = %lf.\n", N, seconds);
     matrix_fill(C, 0);
-    start = clock();
 
+    clock_gettime(CLOCK_MONOTONIC, &t0);
     mul_matrices_cache_friendly2(A, B, C);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
 
-    end = clock();
-    seconds = (float)(end - start) / CLOCKS_PER_SEC;
+    seconds = diff_sec(t1, t0);
 
-    printf("[cache-friendly] N = %d, Elapsed time = %f.\n", N, seconds);
+    printf("[cache-friendly] N = %d, Elapsed time = %lf.\n", N, seconds);
 
     matrix_fill(C, 0);
-    start = clock();
 
+    clock_gettime(CLOCK_MONOTONIC, &t0);
     mul_matrices_cache_friendly_most2(A, B, C);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
 
-    end = clock();
-    seconds = (float)(end - start) / CLOCKS_PER_SEC;
+    seconds = diff_sec(t1, t0);
 
-    printf("[most cache-friendly] N = %d, Elapsed time = %f.\n", N, seconds);
+    printf("[most cache-friendly] N = %d, Elapsed time = %lf.\n", N, seconds);
+
+    matrix_fill(C, 0);
+
+    clock_gettime(CLOCK_MONOTONIC, &t0);
+    mul_matrices_cache_friendly_most_mt(A, B, C,8);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+
+    seconds = diff_sec(t1, t0);
+
+    printf("[Parallel] N = %d, Elapsed time = %lf.\n", N, seconds);
 
     matrix_dtor(C);
     matrix_dtor(B);
